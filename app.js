5cf005054fe31b4ad97fd997248988b8534e38a4
Le Pham <phamtle724@gmail.com>
2022-07-19 15:56:04 -0700

    show gif and get gif function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Let's get this party started!");
+function showGifs(gif) {
+  let $gifV = $(`<img src=${gif}>`);
+  $(".gifs").append($gifV)
+}
+
+async function getGifs() {
+  let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
+  {params: {q: $("input").val() }})
+  showGifs(gif.data[0].url);
+}
