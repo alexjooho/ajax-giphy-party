15147a136268e461374c2df006daea8e810bb4f4
Alex <alexjooho@gmail.com>
2022-07-19 16:53:11 -0700

    Made comments about things we changed

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ async function getGifs() {
   let gifName = $(".search").val()
   let apiKey = "qfVX854X9vKMQ46q6oMfhzmGBXL9lNJn"
 
-  let gif = await axios.get("https://api.giphy.com/v1/gifs/search",
+  let gif = await axios.get("https://api.giphy.com/v1/gifs/search", // need the whole https:// part as well  (whole url)
   {params: {q: gifName, api_key: apiKey}})
   
   return gif.data.data[0].images.original.url
+  // we needed to do data.data and do a specific type of URL that was
+  // not cross origin  (media file)
 
 }
 
-$('.form').on('submit', async function(evt) {
+$('.form').on('submit', async function(evt) {  // ALWAYS need async function with await   // don't forget event listener!
   evt.preventDefault();
   let gifUrl = await getGifs();
 
