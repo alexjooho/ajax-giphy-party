d796424175ed36e1aabf067799173548eb6def43
Alex <alexjooho@gmail.com>
2022-07-19 16:39:21 -0700

    Finished exercise and made sure it worked

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+"use strict";
+// don't forget to host to http first!
+
 console.log("Let's get this party started!");
+
 function showGifs(gif) {
   let $gifV = $(`<img src=${gif}>`);
   $(".gifs").append($gifV)
 }
 
 async function getGifs() {
-  let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
-  {params: {q: $("input").val() }})
-  showGifs(gif.data[0].url);
+  let gifName = $(".search").val()
+  let apiKey = "qfVX854X9vKMQ46q6oMfhzmGBXL9lNJn"
+
+  let gif = await axios.get("https://api.giphy.com/v1/gifs/search",
+  {params: {q: gifName, api_key: apiKey}})
+  
+  return gif.data.data[0].images.original.url
+
 }
+
+$('.form').on('submit', async function(evt) {
+  evt.preventDefault();
+  let gifUrl = await getGifs();
+
+  showGifs(gifUrl);
+});
+
+$('.delete').on('click', function() {
+  $('.gifs').html('');
+})
